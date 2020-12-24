8815f154518557ea2d4533a69a8e224e795343aa
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-24 22:47:33 +0100

    Add posts fetch api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Layout from "../components/layouts/main"
+import { getAllPosts } from "../lib/api"
 
-const HomePage = () => {
+const HomePage = ({ allPosts }) => {
+  const main = allPosts[0]
+  console.log('main', main)
   return (
     <Layout>
-
+      title: {main.title}
+      date: {main.date}
     </Layout>
   )
 }
 export default HomePage;
+
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}

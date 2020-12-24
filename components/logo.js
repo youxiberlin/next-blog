663722d7d1e04b2917240ba36b5dbd3a9dc01977
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-24 22:10:48 +0100

    Add logo

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,25 @@
 const Logo = () => (
-	<span>
-		ykst
-	</span>
+  <span>
+  <svg height="20" width="20">
+    <circle cx="10" cy="10" r="8" stroke="black" stroke-width="1" fill="red" />
+  </svg>
+    ykst
+    <style jsx>{`
+      span {
+        font-weight: bold;
+        font-size: 18px;
+        display: inline-flex;
+        align-items: center;
+        height: 30px;
+        line-height: 20px;
+        padding: 10px;
+      }
+
+      svg {
+        margin-right: 10px;
+      }
+    `}</style>
+  </span>	
 );
 
 export default Logo;

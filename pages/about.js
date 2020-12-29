bf2de21774a1d0f2a9b92784734ef16732efa844
Yuki Sato <yuki.sato@lightcurve.io>
2020-12-29 09:55:37 +0100

    Migrate projects and about data to lib

--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,5 @@
 import Layout from "../components/layouts/main"
-
-const contents = [
-  {
-    year: "2020 -",
-    location: "Berlin",
-    job_title: "Full-stack Engineer",
-    org: "Warashibe",
-    tech: ["Node.js", "Next Dapp", "Next.js", "Firebase"],
-    description: "Reprehenderit nulla culpa culpa quis id ea. Cupidatat in enim aliquip culpa esse sit id occaecat incididunt exercitation cupidatat velit. Qui duis do esse culpa occaecat. Labore exercitation ad ullamco est."
-  },
-  {
-    year: "2018 - 2020",
-    location: "Berlin",
-    job_title: "Backend Engineer",
-    org: "Lisk || Lightcurve",
-    tech: ["Node.js", "PostgresSQL", "Express", "Socket.io", "Swagger"],
-    description: "Reprehenderit nulla culpa culpa quis id ea. Cupidatat in enim aliquip culpa esse sit id occaecat incididunt exercitation cupidatat velit. Qui duis do esse culpa occaecat. Labore exercitation ad ullamco est."
-  },
-  {
-    year: "2012 - 2018",
-    location: "Berlin",
-    job_title: "Tech journalist, Consulting",
-    org: "Freelance",
-    description: "Reprehenderit nulla culpa culpa quis id ea. Cupidatat in enim aliquip culpa esse sit id occaecat incididunt exercitation cupidatat velit. Qui duis do esse culpa occaecat. Labore exercitation ad ullamco est."
-  },
-]
-
-const skill = {
-  general: ["Web development", "Market research", "Translation", "Writing"],
-  lng: ["Engligh", "Japanese(native)", "German"]
-}
+import { contents, skill } from "../lib/data/about"
 
 const Page = ({}) => {
   return (
